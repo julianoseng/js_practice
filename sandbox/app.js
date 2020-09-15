e9141326c70c0829bf732278ac18d65cbfa10bb6
julianoseng <66235816+julianoseng@users.noreply.github.com>
2020-09-15 16:25:21 -0500

    practice

--- sandbox/app.js
+++ sandbox/app.js
@@ -242,3 +242,254 @@ Ctrl + Alt + Enter = insert semicolon and stay at same position
 // val = str.includes('Hello');
 
 // console.log(val);
+
+
+// // 9/15/2020
+// const name = 'John';
+// const age = 30;
+// const job = 'Web Developer';
+// const city = 'Miami';
+// let html;
+
+// // Insert without template strings (es5)
+// html = '<ul><li>Name: ' + name + ' </li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
+
+// // With template strings (es6)
+// html = ` 
+//     <ul>
+//         <li>Name: ${name}</li>
+//         <li>Age: ${age}</li>
+//         <li>Job: ${job}</li>
+//         <li>City: ${city}</li>
+//         <li> ${2 + 2}</li>
+//         <li></li>
+//         <li></li>
+//     </ul>
+// `;
+
+// // Arrays & Array Methods
+
+// const numbers = [1,2,3,4,36,5];
+// const numbers2 = new Array(22,45,23,36,334,78);
+// //  Can be an array of strings
+// const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+// const mixed = [22,'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+
+// let val;
+
+// // Get array length
+// val = numbers.length;
+// // Check if array
+// val = Array.isArray(numbers);
+// // Check single value
+// val = numbers[3];
+// val = numbers[0];
+// // Insert into array
+// numbers[2] = 100;
+// // Find index of value
+// val = numbers.indexOf(36);
+
+// // Mutating arrays
+// // Add on to end
+// numbers.push(250);
+// // Add on to front
+// numbers.unshift(250);
+// // Take off from end
+// numbers.pop();
+// // Take off from front
+// numbers.shift();
+// // Splice values
+// numbers.splice(0,3);
+// // Reverse the array
+// numbers.reverse();
+// // Concatenate Array
+// val = numbers.concat(numbers2);
+// // Sorting arrays
+// val = fruit.sort();
+// val = numbers.sort(); //Sorting by the first number
+// // Use compare function to fix this
+// val = numbers.sort(function(x, y){
+//     return x-y;
+// })
+
+// // Reverse sort
+// val = numbers.sort(function(x, y){
+//     return y-x;
+// })
+
+// // Find
+// function underFifty(num) {
+//     return num < 50;
+// }
+
+// val = numbers.find(underFifty);
+
+
+// console.log(numbers);
+// console.log(val);
+
+
+// // Object literals
+//  const person = {
+//      firstName: 'Steve',
+//      lastName: 'Smith',
+//      age: 30,
+//      email: 'steve@aol.com',
+//      hobbies: ['music', 'sports'],
+//      address: {
+//          city: 'Miami',
+//          state: 'FL'
+//      },
+//      getBirthYear: function() {
+//          return 2017 - this.age;
+//      }
+//  }
+
+//  let val;
+
+//  val = person;
+
+// // Get specific value
+// val = person.firstName;
+// val = person.age;
+// val = person.hobbies[0];
+// val = person.address.state;
+// val = person.address.city;
+// val = person.getBirthYear();
+
+//  console.log(val);
+
+// const people = [
+//     {name: 'John', age:30},
+//     {name: 'Mike', age:23}
+// ];
+
+// for(let i = 0; i < people.length; i++) {
+//     console.log(people[i].name);
+// }
+
+
+
+// // Date object & Times
+
+// let val;
+
+// const today = new Date();
+// // const birthday = new Date('9-10-1982');
+// let birthday = new Date('September 10 1982');
+//                         // 9/10/1981
+// // val = birthday;
+// val = today.getMonth();
+// val = today.getDate();
+// val = today.getDay();
+// val = today.getFullYear();
+// val = today.getHours();
+
+// birthday.setMonth(2);
+// birthday.setDate(12);
+// birthday.setFullYear(1985);
+// birthday.setHours(3);
+// birthday.setMinutes(30);
+// birthday.setSeconds(25);
+
+// console.log(birthday);
+
+
+
+
+// // If statements & Comparison Operators
+
+
+
+// if (something) {
+//     do something
+// } else {
+//     do something else
+// }
+
+const id = '100';
+
+// // equal to
+// if (id == 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT')
+// }
+
+// // Not equal to
+// if (id != 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT')
+// }
+
+// // Equal to value and type
+// if (id === 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT')
+// }
+
+// // Not equal to value and type
+// if (id !== 100) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT')
+// }
+
+// // Check if something is undefined
+// if(typeof id !== 'undefined'){
+//     console.log(`The ID is ${id}`);
+// } else {
+//     console.log('NO ID');
+// }
+
+// // Greater or less than
+// if (id > 200) {
+//     console.log('CORRECT');
+// } else {
+//     console.log('INCORRECT')
+// }
+
+// // If Else
+// const color = 'yellow';
+
+// if(color === 'red'){
+//     console.log('Color is red');
+// } else if(color === 'blue'){
+//     console.log('Color is blue');
+// } else {
+//     console.log('Color is not red or blue.');
+// }
+
+// // Logical operators
+
+// const name = 'Steve';
+// const age = 25;
+
+// // And &&
+// if(age > 0 && age < 12){
+//     console.log(`${name} is a child.`);
+// } else if(age > 13 && age <= 19) {
+//     console.log(`${name} is a teenager.`);
+// } else {
+//     console.log(`${name} is an adult.`)
+// }
+
+// // Or ||
+// if(age < 16 || age >65){
+//     console.log(`${name} can not run in the race.`)
+// } else {
+//     console.log(`${name} is registered for the race.`)
+// }
+
+
+
+// // Ternary operator
+// console.log(id === 100 ? 'Correct' : 'Incorrect');
+
+// // Without braces
+// if(id === 100)
+//     console.log('Correct');
+// else
+//     console.log('Incorrect');
